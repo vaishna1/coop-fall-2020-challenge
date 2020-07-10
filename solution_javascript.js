3fefed4c365e5b43b0f343e5c56652f45fd8239e
vaishna1 <58011491+vaishna1@users.noreply.github.com>
2020-07-10 14:25:17 -0400

    Update solution_javascript.js

--- solution_javascript.js
+++ solution_javascript.js
@@ -2,14 +2,72 @@ class EventSourcer {
   constructor() {
     this.value = 0;
   }
-
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  
+function Item (perform, data) {
+	this.perform = perform;
+	this.data = data;
 }
+
+function Stack(self) {
+	this.stack = [];
+	this.current = -1;
+	this.self = self;
+}
+
+  add(num) {
+    Stack.prototype.push = function (perform, data) {
+	  this.current++;
+    this.stack.splice(this.current);
+	  this.stack.push(new Item(perform, data));
+  }
+  subtract(num) {
+    Stack.prototype.pop = function (perform, data) {
+	  this.current--;
+    this.stack.splice(this.current);
+	  this.stack.pop(new Item(perform, data));
+  
+  }
+ 
+  Stack.prototype.undo = undo(){
+	var item;
+
+	if (this.current >= 0) {
+		item = this.stack[this.current];
+		item.perform.call(this.self, false, item.data);
+		this.current--;
+	  } 
+    else 
+    {
+		throw new Error("Already at oldest change");
+	  }
+  };
+  
+  
+  Stack.prototype.redo = redo() {
+	var item;
+
+	item = this.stack[this.current + 1];
+	if (item) {
+		item.perform.call(this.self, true, item.data);
+		this.current++;
+	  } else 
+    {
+		throw new Error("Already at newest change");
+	  }
+  };
+  
+  bulk_undo(num) {
+    for(i=0; i<num; i++ )
+    {
+      Stack.prototype.undo = undo(); 
+    }
+   }
+  bulk_redo(num) {
+    for(i=0; i<num; i++ )
+     {
+        Stack.prototype.redo = redo(); 
+      }
+   }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
 module.exports = EventSourcer;
